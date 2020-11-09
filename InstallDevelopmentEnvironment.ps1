6c4a6473a039e02075bb76b0fda21f7f11ba3b7c
mu88 <4560672+mu88@users.noreply.github.com>
2020-11-09 17:49:00 +0100

    Update InstallDevelopmentEnvironment.ps1

--- InstallDevelopmentEnvironment.ps1
+++ InstallDevelopmentEnvironment.ps1
@@ -8,10 +8,13 @@ choco install docker-desktop -fy
 
 # Version Control System
 choco install git -fy
+choco install winmerge -fy
 choco install gitextensions -fy
 
 # IDE and essential development
 choco install nodejs -fy
+choco install firacode -y
+choco install notepadplusplus -fy
 choco install vscode -fy
 choco install visualstudio2019enterprise -fy --params "--locale en-US --add Microsoft.VisualStudio.Workload.NetCoreTools;Microsoft.VisualStudio.Workload.NetWeb;includeRecommended --passive"
 
@@ -25,8 +28,6 @@ choco install sql-server-management-studio -fy
 choco install powertoys -fy
 choco install windirstat -fy
 choco install sysinternals -fy
-choco install notepadplusplus -fy
-choco install winmerge -fy
 choco install joplin -fy
 choco install ditto -fy
 choco install agentransack -fy
