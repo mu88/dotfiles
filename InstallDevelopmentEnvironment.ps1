2c53f1801218170880ffdd79c135e644a2c68a5e
mu88 <4560672+mu88@users.noreply.github.com>
2020-11-09 14:14:31 +0100

    Create InstallDevelopmentEnvironment.ps1

---
+++ InstallDevelopmentEnvironment.ps1
@@ -0,0 +1,36 @@
+# Browsers
+choco install firefox -fy
+choco install googlechrome -fy
+choco install tor-browser -fy
+
+# Container
+choco install docker-desktop -fy
+
+# Version Control System
+choco install git -fy
+choco install gitextensions -fy
+
+# IDE and essential development
+choco install nodejs -fy
+choco install vscode -fy
+choco install visualstudio2019enterprise -fy --params "--locale en-US --add Microsoft.VisualStudio.Workload.NetCoreTools;Microsoft.VisualStudio.Workload.NetWeb;includeRecommended --passive"
+
+# Additional Debuggers
+choco install fiddler -fy
+
+# Database Tools
+choco install sql-server-management-studio -fy
+
+# Helpers
+choco install powertoys -fy
+choco install windirstat -fy
+choco install sysinternals -fy
+choco install notepadplusplus -fy
+choco install joplin -fy
+choco install ditto -fy
+choco install agentransack -fy
+choco install veracrypt -fy
+choco install adobereader -fy
+choco install vlc -fy
+choco install 7zip -fy
+choco install paint.net -fy
