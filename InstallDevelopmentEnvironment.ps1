4fd2d26ec32c3dcb62c87e9322935f0e94c15315
mu88 <4560672+mu88@users.noreply.github.com>
2021-04-14 09:22:16 +0200

    Update InstallDevelopmentEnvironment.ps1

--- InstallDevelopmentEnvironment.ps1
+++ InstallDevelopmentEnvironment.ps1
@@ -13,14 +13,21 @@ choco install gitextensions -fy
 
 # IDE and essential development
 choco install nodejs -fy
-choco install firacode -y
 choco install notepadplusplus -fy
 choco install vscode -fy
 choco install visualstudio2019enterprise -fy --params "--locale en-US --add Microsoft.VisualStudio.Workload.NetCoreTools;Microsoft.VisualStudio.Workload.NetWeb;includeRecommended --passive"
+choco install jetbrains-rider -fy
 
-# Web development
+# Shell / Terminal
+choco install powershell-core -fy
+choco install firacode -fy
+choco install firacodenf -fy
+
+# Web development / network analysis
 choco install fiddler -fy
 choco install postman -fy
+choco install wireshark -fy
+choco install winpcap -fy
 
 # Database Tools
 choco install sql-server-management-studio -fy
@@ -40,3 +47,5 @@ choco install vlc -fy
 choco install screenpresso -fy
 choco install 7zip -fy
 choco install paint.net -fy
+choco install powertoys -fy
+choco install winscp -fy
