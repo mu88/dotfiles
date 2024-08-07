fc1075806787682bcacc5d6e019a437805b8e359
mu88 <4560672+mu88@users.noreply.github.com>
2024-08-07 10:52:09 +0200

    feat: don't force installation of Chocolatey packages

--- InstallDevelopmentEnvironment.ps1
+++ InstallDevelopmentEnvironment.ps1
@@ -1,72 +1,72 @@
 # Browsers
-choco install firefox -fy
-choco install googlechrome -fy
-choco install tor-browser -fy
+choco install firefox -y
+choco install googlechrome -y
+choco install tor-browser -y
 
 # Container
-choco install docker-desktop -fy
+choco install docker-desktop -y
 
 # Version Control System
-choco install git -fy
-choco install winmerge -fy
-choco install gitextensions -fy
-choco install gh -fy
+choco install git -y
+choco install winmerge -y
+choco install gitextensions -y
+choco install gh -y
 
 # IDE and essential development
-choco install nodejs -fy
-choco install dotnet-sdk -fy
-choco install notepadplusplus -fy
-choco install vscode -fy
-choco install visualstudio2022professional -fy --params "--locale en-US"
-choco install jetbrainstoolbox -fy # consider adding '--ia "/D=C:\new\path"' as otherwise the toolbox will be installed to %LOCALAPPDATA%
+choco install nodejs -y
+choco install dotnet-sdk -y
+choco install notepadplusplus -y
+choco install vscode -y
+choco install visualstudio2022professional -y --params "--locale en-US"
+choco install jetbrainstoolbox -y # consider adding '--ia "/D=C:\new\path"' as otherwise the toolbox will be installed to %LOCALAPPDATA%
 
 # Additional development tools
-choco install nunit-console-runner -fy
-choco install nuget.commandline -fy
-choco install nugetpackageexplorer -fy
+choco install nunit-console-runner -y
+choco install nuget.commandline -y
+choco install nugetpackageexplorer -y
 
 # Shell / Terminal
-choco install microsoft-windows-terminal -fy
-choco install powershell-core -fy
-choco install firacode -fy
-choco install firacodenf -fy
+choco install microsoft-windows-terminal -y
+choco install powershell-core -y
+choco install firacode -y
+choco install firacodenf -y
 choco install oh-my-posh
 
 # Web development / network analysis
-choco install fiddler -fy
-choco install postman -fy
-choco install wireshark -fy
-choco install winpcap -fy
+choco install fiddler -y
+choco install postman -y
+choco install wireshark -y
+choco install winpcap -y
 
 # Cloud tools
-choco install awscli -fy
-choco install kustomize -fy
+choco install awscli -y
+choco install kustomize -y
 
 # Database Tools
-choco install sql-server-management-studio -fy
-choco install pgadmin4 -fy
-choco install sqlitebrowser -fy
+choco install sql-server-management-studio -y
+choco install pgadmin4 -y
+choco install sqlitebrowser -y
 
 # Helpers
-choco install 7zip -fy
-choco install agentransack -fy
-choco install ditto -fy
-choco install eraser -fy
-choco install joplin -fy
-choco install keepass -fy
-choco install keepass-plugin-kpscript -fy
-choco install paint.net -fy
-choco install pdfcreator -fy
-choco install powertoys -fy
-choco install screenpresso -fy
-choco install sysinternals -fy
-choco install veracrypt -fy
-choco install vlc -fy
-choco install windirstat -fy
-choco install winscp -fy
+choco install 7zip -y
+choco install agentransack -y
+choco install ditto -y
+choco install eraser -y
+choco install joplin -y
+choco install keepass -y
+choco install keepass-plugin-kpscript -y
+choco install paint.net -y
+choco install pdfcreator -y
+choco install powertoys -y
+choco install screenpresso -y
+choco install sysinternals -y
+choco install veracrypt -y
+choco install vlc -y
+choco install windirstat -y
+choco install winscp -y
 
 # Others
-choco install stretchly -fy
+choco install stretchly -y
 
 Install-Module PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
 Install-Module posh-git -Scope CurrentUser
